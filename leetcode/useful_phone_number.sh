c282dffee88a61f2caef94a23c539d4f9aa701d7
fenshitianyue <abzanda@163.com>
2019-04-08 09:35:05 +0800

    add the method

--- leetcode/useful_phone_number.sh
+++ leetcode/useful_phone_number.sh
@@ -6,6 +6,17 @@
 # 123 789 891
 # (123) 456-7890 # matched
 
-grep -P '^(\(\d{3}\) |\d{3}-)\d{3}-\d{4}$' file.txt
+phone_number="^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$"
+
+while read line 
+do 
+  if [[ $line =~ $phone_number ]]
+  then 
+    echo $line
+  fi
+done <file.txt
+
+# else
+# grep -P '^(\(\d{3}\) |\d{3}-)\d{3}-\d{4}$' file.txt
 # else
 # grep '^\(([0-9]\{3\}) \|[0-9]\{3\}-\)[0-9]\{3\}-\{4\}$' file.txt
