864c151906a1cda90bae68e64984e1911ffaf1a0
fenshitianyue <abzanda@163.com>
2019-05-23 12:59:53 +0800

    Programming problem number five

---
+++ nowcoder/bit_5.cc
@@ -0,0 +1,54 @@
+//找出n个数里出现次数大于等于n/2的数
+
+//case 1:
+//对数组排序，然后找出中间那个数
+
+//case 2:
+//因为要找过半的数，用一个变量count记录读取每个变量变化的次数，一个变量temp记录可能过
+//半的数，先让count=1，然后让temp=vec[0]，然后往后遍历一遍，碰到和temp相同的数就给count++，否
+//则就count--，如果，count变成0，就让temp=vec[i](vec数组遍历过程中的当前值),并让count=1，如此遍历
+//一遍，因为有一个数过半，所以temp最后肯定存储的是过半的数
+
+//case 3:
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void case_1(vector<int>& v){
+  sort(v.begin(), v.end());
+  cout << v[(v.size() >> 1) - 1];
+}
+
+void case_2(vector<int>& v){
+  int count = 1;
+  int tmp = v[0];
+  for(size_t i = 1; i != v.size(); ++i){
+    if(v[i] == tmp)
+      ++count;
+    else 
+      --count;
+    if(0 == count){
+      tmp = v[i];
+      ++count;
+    }
+  }
+  cout << tmp << endl;
+}
+
+void case_3(vector<int>& v){
+
+}
+
+
+int main() {
+  int n = 0;
+  vector<int> v;
+  while(cin >> n){
+    v.push_back(n);
+  } 
+  //case_1(v);
+  return 0;
+}
