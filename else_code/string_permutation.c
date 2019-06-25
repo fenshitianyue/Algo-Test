d39b10dd3641812a0b483e3959d4b7387b409e69
fenshitianyue <abzanda@163.com>
2019-06-25 17:37:28 +0800

    string permutation

---
+++ else_code/string_permutation.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(char* c1, char* c2){
+  char tmp = *c1;
+  *c1 = *c2;
+  *c2 = tmp;
+}
+
+void permutation(char* str, char* pStart){
+  if(!str || !pStart){
+    return;
+  }
+  char* p = pStart;
+  if(*pStart == '\0'){
+    printf("%s  ", str);
+  }else{
+    for(p = pStart; *p != '\0'; ++p){
+      swap(pStart, p);
+      permutation(str, pStart + 1);
+      swap(pStart, p);
+    }
+  }
+
+}
+
+int main() {
+  
+  return 0;
+}
