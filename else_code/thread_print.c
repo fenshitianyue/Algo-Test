0eb3cdb7415cd83585741e7a6066388b19558e1d
fenshitianyue <abzanda@163.com>
2019-06-25 20:40:15 +0800

    add a programming questions about threads

---
+++ else_code/thread_print.c
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////
+// 创建两个线程，一个打印元音字母，一个打印辅音字母
+///////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include <pthread.h>
+
+
+char array[27] = "abcdefghijklmnopqrstuvwxyz";
+int index = 0;
+
+void PrintVowels(char* array){
+  
+}
+
+void PrintConsonants(char* array){
+
+}
+
+int main() {
+  PrintVowels(array); 
+  PrintConsonants(array);
+  return 0;
+}
+
